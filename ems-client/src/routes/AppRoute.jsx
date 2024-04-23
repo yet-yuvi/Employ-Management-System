bec6f5bab5aa901c58c77855801d63d4b6c8be8a
Jubayar Ahamad <oscyanogen@gmail.com>
2024-04-23 23:09:17 +0600

    testing remote git

--- ems-client/src/routes/AppRoute.jsx
+++ ems-client/src/routes/AppRoute.jsx
@@ -13,6 +13,10 @@ const router = createBrowserRouter([
         element: <Home />
       },
       {
+        path:'/home',
+        element: <Home />
+      },
+      {
         path: '/about',
         element: <About />
       },
