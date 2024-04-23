a5a63c2c86e3dec18a39460463be7343e59a5e4a
Jubayar Ahamad <oscyanogen@gmail.com>
2024-04-24 05:31:31 +0600

    Add toggling functionality for NavBar

--- ems-client/src/components/bannerBar/BannerBar.jsx
+++ ems-client/src/components/bannerBar/BannerBar.jsx
@@ -1,19 +1,30 @@
-import React from "react";
 import { ReactComponent as BarIcon } from "../../icons/bar.svg";
 import emsLogo from "../../logo/emsLogo.jpg";
 
-export const BannerBar = () => {
+
+export const BannerBar = ({toggleNavbar}) => {
+
   return (
     <div className="flex relative px-4 w-full h-[70px] bg-blue-400 items-center ">
       <div className="flex items-center">
-        <button className="size-6 items-center mx-4 color-white">
+        <button
+          onClick={() => toggleNavbar()}
+          className="size-6 items-center mx-4 color-white ring-4 ring-red-300 z-40"
+        >
           <BarIcon />
         </button>
-        <img className="h-10 w-10 mx-2 rounded-full" src={emsLogo} alt="" />
+        <img
+          className="h-10 w-10 mx-2 rounded-full ring-4 ring-gray-400"
+          src={emsLogo}
+          alt=""
+        />
       </div>
       <div className="flex absolute items-center justify-center w-full">
-        <p className="text-white font-medium text-2xl">Employ Management System</p>
+        <p className="text-white font-medium text-2xl">
+          Employ Management System
+        </p>
       </div>
+      
     </div>
   );
 };
--- ems-client/src/pages/Base.jsx
+++ ems-client/src/pages/Base.jsx
@@ -1,15 +1,24 @@
 import { Outlet } from "react-router-dom";
 import { BannerBar, NavBar } from "../components";
+import { useState } from "react";
 
 export const Base = () => {
+
+  const [openNavbar, setOpenNavbar] = useState(false);
+
+  const toggleNavbar = (event) => {
+    console.log("toggling");
+    setOpenNavbar((prevOpenNavbar) => !prevOpenNavbar); 
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-50">
-        <BannerBar />
+        <BannerBar toggleNavbar={toggleNavbar}/>
       </div>
       <div className="flex h-full mt-[70px]">
-        <div className="h-full">
-          <NavBar />
+        <div className="h-full ">
+        {openNavbar && <NavBar />}
         </div>
         <div>
           <Outlet />
