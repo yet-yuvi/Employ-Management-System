e46e85b5d70d7e65855b95f5fc175cce9028dbd7
Jubayar Ahamad <oscyanogen@gmail.com>
2024-04-24 22:00:55 +0600

    Add banner bar logo route and SignIn, SignUp page route

--- ems-client/src/components/bannerBar/BannerBar.jsx
+++ ems-client/src/components/bannerBar/BannerBar.jsx
@@ -1,21 +1,27 @@
 import { ReactComponent as BarIcon } from "../../icons/bar.svg";
 import emsLogo from "../../logo/emsLogo.jpg";
+import { useNavigate } from "react-router-dom";
 
 
 export const BannerBar = ({toggleNavbar}) => {
+  const navigate = useNavigate();
 
+  const handleLogoClick = () => {
+    navigate("/");
+  }
   return (
-    <div className="flex relative px-4 w-full h-[70px] bg-blue-400 items-center ">
+    <div className="flex px-4 w-full h-[70px] bg-blue-400 items-center ">
       <div className="flex items-center">
         <button
           onClick={() => toggleNavbar()}
-          className="size-6 items-center mx-4 color-white ring-4 ring-red-300 z-40"
+          className="size-6 items-center mx-4 color-white  z-40"
         >
           <BarIcon />
         </button>
         <img
-          className="h-10 w-10 mx-2 rounded-full ring-4 ring-gray-400"
+          className="h-10 w-10 mx-2 cursor-pointer rounded-full ring-4 ring-gray-400 z-40"
           src={emsLogo}
+          onClick={() => handleLogoClick()}
           alt=""
         />
       </div>
