1e421bf97ce559fbf0f3d237cb466af4909d2cde
Jubayar Ahamad <oscyanogen@gmail.com>
2024-05-04 00:44:16 +0600

    Table design

--- ems-client/src/components/employees/EmployeesList.jsx
+++ ems-client/src/components/employees/EmployeesList.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Table } from "../table/Table";
 
 export const EmployeesList = () => {
   const headers = ["SL", "Name", "Job", "Favorite Color"];
   const columns = [
-    [1, "Cy Ganderton", "Quality Control Specialist", "Blue", {name: "Edit", user: {}}],
+    [1, "Cy Ganderton", "Quality Control Specialist", "Blue"],
     [2, "Cy Ganderton", "Quality Control Specialist", "Blue"],
     [3, "Cy Ganderton", "Quality Control Specialist", "Blue"],
     [4, "Cy Ganderton", "Quality Control Specialist", "Blue"],
   ];
+  useEffect(() => {
+    for (let item of columns) {
+      if (true) {
+        item.push({ name: "Edit", user: {} });
+      }
+    }
+    if (true) {
+      headers.push("Detail Button");
+    }
+  });
   return (
-    <div>
-      <Table headers={headers} columns={columns} />
+    <div className="w-full flex justify-center m-2">
+      <Table
+        headers={headers}
+        columns={columns}
+        banner={"Employees"}
+        banner_color={"black"}
+        header_color={"gray-700"}
+        cell_color={"gray-300"}
+        button_color={"cyan-300"}
+      />
     </div>
   );
 };
-
 
 // const data = {
 //   sl: 1,
